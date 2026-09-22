ea4edbba457cc384daccda18eb0b53cba9c00589
Jaid <6216144+Jaid@users.noreply.github.com>
2026-09-22 19:49:13 +0100

    Applied ESLint code fixes

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,6 @@
 import {expect, test} from 'bun:test'
 
 const {default: decodeZstd} = await import('#src/main.ts')
-
 test('should run', () => {
   const result = decodeZstd()
   expect(result).toBe('decode-zstd') // TODO Test actual functionality
